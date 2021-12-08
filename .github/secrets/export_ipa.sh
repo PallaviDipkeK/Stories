e0c8e1bd813329657268395049eb18f89fcf2947
Pallavi <Pallavi.dipke@bankopen.co>
2021-12-08 22:44:29 +0530

    sdcfvgbvcxz

--- .github/secrets/export_ipa.sh
+++ .github/secrets/export_ipa.sh
@@ -2,8 +2,8 @@
 
 set -eo pipefail
 
-xcodebuild -archivePath $PWD/build/Calculator.xcarchive \
-            -exportOptionsPlist Calculator-iOS/Calculator\ iOS/exportOptions.plist \
+xcodebuild -archivePath $PWD/build/StoriesSDK.xcarchive \
+            -exportOptionsPlist StoriesSDK-iOS/StoriesSDK\ iOS/exportOptions.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
